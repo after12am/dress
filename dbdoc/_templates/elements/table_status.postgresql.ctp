bbdda6cd0b8696a73803d76fb7609038c45b0530
after12am <me.after.12am@gmail.com>
2014-08-26 00:09:47 +0900

    fix #20

--- dbdoc/_templates/elements/table_status.postgresql.ctp
+++ dbdoc/_templates/elements/table_status.postgresql.ctp
@@ -6,6 +6,7 @@
         <th>Null</th>
         <th>Key</th>
         <th>Default</th>
+        <th>Extra</th>
         <th>Comment</th>
     </tr>
     {% for c in item.columns %}
@@ -17,6 +18,7 @@
         <td>{% if c[8] %}{{c[8]}}{% endif %}</td>
         <td>{{c[5]}}</td>
         <td>{{c[6]}}</td>
+        <td>{{c[7]}}</td>
     </tr>
     {% endfor %}
 </table>
--- dbdoc/datasource.py
+++ dbdoc/datasource.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-import os, sys
+import os, sys, re
 
 class MySQL(object):
     
@@ -95,6 +95,69 @@ class SQLite3(object):
 
 class PostgreSQL(object):
     
+    _get_columns_query = """
+        select
+            column_name,
+            udt_name,
+            character_maximum_length,
+            collation_name,
+            is_nullable,
+            column_default
+        from
+            information_schema.columns
+        where
+            table_catalog = '%s'
+        and table_name = '%s'
+        order by ordinal_position;
+    """
+    
+    _get_comment_query = """
+        select
+            pd.description
+        from
+            pg_stat_all_tables as psat
+        inner join
+            pg_description as pd on psat.relid = pd.objoid
+        inner join
+            pg_attribute as pa on pd.objoid = pa.attrelid and pd.objsubid = pa.attnum
+        where
+            psat.schemaname = (select schemaname from pg_stat_user_tables where relname = '%s')
+        and psat.relname = '%s'
+        and pa.attname = '%s'
+        and pd.objsubid <> 0
+        order by pd.objsubid;
+    """
+    
+    _get_key_query = """
+        select
+            tc.constraint_name,
+            tc.constraint_type
+        from
+        	information_schema.table_constraints as tc
+        inner join
+            information_schema.constraint_column_usage as ccu on 
+                tc.table_catalog = ccu.table_catalog 
+            and tc.table_schema = ccu.table_schema 
+            and tc.table_name = ccu.table_name 
+            and tc.constraint_name = ccu.constraint_name
+        where
+        	tc.table_catalog = '%s'
+        and tc.table_name = '%s'
+        and ccu.column_name = '%s'
+    """
+    
+    _get_table_comment_query = """
+        select
+            pd.description
+        from
+            pg_stat_user_tables as psut
+        inner join
+            pg_description as pd on psut.relid = pd.objoid
+        where
+            psut.relname = '%s'
+        and pd.objsubid = 0;
+    """
+    
     def __init__(self, host, user, password, database):
         self.conn = None
         self.cursor = None
@@ -123,69 +186,35 @@ class PostgreSQL(object):
         return sorted([item[0] for item in self.cursor.fetchall()])
     
     def get_columns(self, table):
-        get_columns_query = """
-            select
-                column_name,
-                udt_name,
-                character_maximum_length,
-                collation_name,
-                is_nullable,
-                column_default
-            from
-                information_schema.columns
-            where
-                table_catalog = '%s'
-            and table_name = '%s'
-            order by ordinal_position;
-        """
-        
-        get_comment_query = """
-            select
-                pd.description
-            from
-                pg_stat_all_tables as psat
-            inner join
-                pg_description as pd on psat.relid = pd.objoid
-            inner join
-                pg_attribute as pa on pd.objoid = pa.attrelid and pd.objsubid = pa.attnum
-            where
-                psat.schemaname = (select schemaname from pg_stat_user_tables where relname = '%s')
-            and psat.relname = '%s'
-            and pa.attname = '%s'
-            and pd.objsubid <> 0
-            order by pd.objsubid;
-        """
-        
-        get_key_query = """
-            select
-                tc.constraint_name,
-                tc.constraint_type
-            from
-            	information_schema.table_constraints as tc
-            inner join
-                information_schema.constraint_column_usage as ccu on 
-                    tc.table_catalog = ccu.table_catalog 
-                and tc.table_schema = ccu.table_schema 
-                and tc.table_name = ccu.table_name 
-                and tc.constraint_name = ccu.constraint_name
-            where
-            	tc.table_catalog = '%s'
-            and tc.table_name = '%s'
-            and ccu.column_name = '%s'
-        """
-        
-        self.cursor.execute(get_columns_query % (self.database, table))
+        self.cursor.execute(self._get_columns_query % (self.database, table))
         status = self.cursor.fetchall()
-        
+        # convert to signage of MySQL
+        for i, x in enumerate(status):
+            status[i].append('')
+            if x[-2] is None:
+                continue
+            if re.match(r"^nextval", x[-2]):
+                status[i][-2] = None
+                # add additional info as Extra
+                status[i][-1] = 'auto_increment'
+                continue
+            m = re.match(r"^'(.*)'::", x[-2])
+            if m:
+                status[i][-2] = m.group(1)
+                continue
+            m = re.match(r"^(.*)::", x[-2])
+            if m:
+                status[i][-2] = m.group(1)
+                continue
         # add additional data, comment and key, to basic column data
         for i, item in enumerate(status):
             column = item[0]
             # merge column comment
-            self.cursor.execute(get_comment_query % (table, table, column))
+            self.cursor.execute(self._get_comment_query % (table, table, column))
             ret = self.cursor.fetchone()
             status[i] += ret if ret is not None else ['']
             # merge key information
-            self.cursor.execute(get_key_query % (self.database, table, column))
+            self.cursor.execute(self._get_key_query % (self.database, table, column))
             ret = self.cursor.fetchone()
             status[i] += ret if ret is not None else [None, '']
         # if multiple unique index, change key name to multiple
@@ -200,18 +229,7 @@ class PostgreSQL(object):
         pass
     
     def get_table_comment(self, table):
-        query = """
-            select
-                pd.description
-            from
-                pg_stat_user_tables as psut
-            inner join
-                pg_description as pd on psut.relid = pd.objoid
-            where
-                psut.relname = '%s'
-            and pd.objsubid = 0;
-        """
-        self.cursor.execute(query % table)
+        self.cursor.execute(self._get_table_comment_query % table)
         ret = self.cursor.fetchall()
         return ret[0][-1]
     
